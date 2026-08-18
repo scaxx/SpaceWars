453a6d1167b5d5bde79c72b65cad7db4ff3aa933
Sofía <danessesofia@gmail.com>
2026-08-17 22:33:47 -0300

    Arreglo manejo usuarios

--- users.py
+++ users.py
@@ -3,11 +3,28 @@ import json
 def loadUsers():
 
     with open("users.json") as users:
+
         usersDict = json.load(users)
 
     return usersDict
 
 def saveUsers(usersDict):
+
     with open("users.json", "w") as users:
+
         json.dump(usersDict, users)
 
+def userExists(username, users):
+
+    return username in users
+
+def createUser(username, users):
+
+    if not userExists(username, users):
+        
+        users[username] = ({
+            "total coins" : 0,
+            "unlockedSkins" : ["default"],
+            "equippedSkins": ["default"],
+            "scores": []
+        })
